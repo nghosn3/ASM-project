1f6819408ea88bc13231f2bb473958f90d5dc4d8
nghosn3 <67711839+nghosn3@users.noreply.github.com>
2022-11-09 15:13:52 -0500

    Add files via upload

--- aed_dose_modeling/figures_code/bootstrapped_log_reg_allAEDs.m
+++ aed_dose_modeling/figures_code/bootstrapped_log_reg_allAEDs.m
@@ -1,6 +1,18 @@
 %% run bootstrapped regression for all patients and all drugs
 close all;clear;
 
+for_borel=0;
+if for_borel
+    addpath('/mnt/leif/littlab/users/nghosn3/Pioneer/spikes-AED/aed_dose_modeling/figures_code')
+    addpath('/mnt/leif/littlab/users/nghosn3/Pioneer/DATA');
+    addpath('/mnt/leif/littlab/users/nghosn3/Pioneer/spikes-AED/aed_dose_modeling')
+    
+else
+    
+    addpath('/Volumes/USERS/nghosn3/Pioneer/spikes-AED/aed_dose_modeling/figures_code')
+    addpath('/Volumes/USERS/nghosn3/Pioneer/DATA');
+    addpath('/Volumes/USERS/nghosn3/Pioneer/spikes-AED/aed_dose_modeling')
+end
 % Get which patients
 cohort_info = readtable('HUP_implant_dates.xlsx');
 ptIDs = cohort_info.ptID;
@@ -20,13 +32,13 @@ if tapered
 end
 
 % using the AED BPL model
-[all_dose_curves,all_tHr,ptIDs,all_med_names,ieeg_offset,max_dur,emu_dur] = get_aed_curve_kg(ptIDs,weights,0,[]); % use min dose to normalize dose
+[all_dose_curves,all_tHr,ptIDs,all_med_names,ieeg_offset,max_dur,emu_dur] = get_aed_curve_kg(ptIDs,weights,0,for_borel); % use min dose to normalize dose
 
 % using only interpolation dosage
 %[all_dose_curves,all_tHr,all_med_names,ieeg_offset,emu_dur] = get_dose_schedules(ptIDs);
 
 %%
-exclude_ativan=0;
+exclude_ativan=1;
 rm_clusters =0;
 
 medications = vertcat(all_med_names{:});
@@ -37,7 +49,7 @@ if exclude_ativan
 end
 
 % make matrix with all variables (levels for each drug)
-num_feats=length(all_medications)+4; % (baseline sz freq, time since last seizure, ptID, seizure bin)
+num_feats=length(all_medications)+3; % ( time since last seizure, ptID, seizure bin)
 sz=num_feats;
 features = cell(num_feats,length(ptIDs));
 winLen =60; %minutes
@@ -64,13 +76,19 @@ for i=1:length(ptIDs) % exclude HUP136
     end_times = cellfun(@(x)x(end),tHr);
     curve_lens = cellfun(@length,pt_curves);
     max_time = max(end_times);
-    drugs =zeros(length(med_names),ceil(max_time*60))+1e-10;
+    drugs =nan(length(med_names),ceil(max_time*60));
     for n =1:length(med_names)
         drug=pt_curves{n};
         drug=drug./nanmax(drug); %normalize drug curve
         if ~isempty(drug)
             dStart = round(tHr{n}(1)*60)-1;
             drugs(n,dStart+1:dStart+length(drug))=drug;
+            % until the drug starts, replace with steady state level unless ativan - for ativan, make it 0
+            if contains(med_names(n),'lorazepam')
+                drugs(n,1:dStart) = zero(1,dStart);
+            elseif dStart >24 % if the drug was administered later in the EMU stay; otherwise keep as nan
+                drugs(n,24:dStart) = zeros(1,dStart-24+1);
+            end
         end
     end
     
@@ -92,7 +110,7 @@ for i=1:length(ptIDs) % exclude HUP136
     end
     
     empty_inds =cellfun(@isempty,features(:,i));
-    features(empty_inds,i)={zeros(1,nbins)+1e-10}; %adding small number to make columns full rank
+    features(empty_inds,i)={zeros(1,nbins)}; %adding small number to make columns full rank
     features(end-1,i)={i*ones(1,nbins)}; %ptID
     
     [seizure_times] = convert_sz_to_emu_time(offsets,ieeg_offset_datasets,ptID); % seizure times in hours of EMU stay
@@ -126,14 +144,21 @@ for i=1:length(ptIDs) % exclude HUP136
         end
     end
     t_last_seizure(j+1:end) = (j+1:length(t_last_seizure))-seizure_inds(end);
+    
+    % assume last seizure happened 'first_sz' hours before emu stay and add that in
+    % beginning
+    nan_inds = find(isnan(t_last_seizure));
+    first_sz = -24*2; 
+    first_inds = nan_inds - first_sz;
+    t_last_seizure(nan_inds)=first_inds;
     features(end-2,i)={t_last_seizure};
     
     % add baseline seizure frequency
     
-    ind = baseline_sz_freqs.HUP_ID == ptIDs(i);
-    this_sz_freq = baseline_sz_freqs.sz_per_month(ind);
-    pt_baseline_sz_frequency = this_sz_freq*ones(1,nbins);
-    features(end-3,i)={pt_baseline_sz_frequency};
+%     ind = baseline_sz_freqs.HUP_ID == ptIDs(i);
+%     this_sz_freq = baseline_sz_freqs.sz_per_month(ind);
+%     pt_baseline_sz_frequency = this_sz_freq*ones(1,nbins);
+%     features(end-3,i)={pt_baseline_sz_frequency};
     
     if rm_clusters
         % remove clustered seizures - more than Xhrs apart
@@ -161,7 +186,7 @@ for n =1:length(all_medications)
     med_counts{n}=sum(contains(medications,all_medications{n}));
 end
 med_info = [all_medications med_counts'];
-exclude_meds = [med_counts{:}]<5;
+exclude_meds = [med_counts{:}]<10;
 all_medications(contains(all_medications,'valproic acid'))={'valproic_acid'};
 
 top_meds=all_medications(~exclude_meds);
@@ -171,7 +196,7 @@ med_feats(exclude_meds,:)=[];
 
 % make table for regression
 features = med_feats;
-feat_names = [top_meds;{'baseline_sz_freq'};{'t_implant'}; {'ptID'}; {'seizure'}];
+feat_names = [top_meds;{'t_last_seizure'}; {'ptID'}; {'seizure'}];
 drug_table = table();
 for i =1:length(feat_names)
     feat_name = feat_names{i};
@@ -181,10 +206,12 @@ for i =1:length(feat_names)
 end
 drug_table.ptID = categorical(drug_table.ptID);
 
-%% run with fake seizure times?
+%% run with fake seizure times? remove rows that contain nan values?
 
 run_with_rand_sz = 0;
 run_with_shifted_sz = 0;
+
+remove_nan_rows = 1;
 
 if run_with_rand_sz
     for i=1:length(pts)
@@ -212,20 +239,28 @@ elseif run_with_shifted_sz
     
 end
 
+% remove nan rows to check if glm is doing that - does not change results (11/09/22)
+if remove_nan_rows
+    array_drug_table = table2array(drug_table(:,top_meds));
+    nan_inds = isnan(array_drug_table);
+    nan_rows = sum(nan_inds,2); nan_rows = nan_rows>0;
+    drug_table(nan_rows,:)=[];
+end
+
 %% run bootstrapped logistic regression, get bootstrapped statistics - for null and AED model
 
 % train model with bootstrapped samples, get bootstrapped estimates/odds ratio
-iter = 1e3; % this takes awhile to run
+iter = 1e1;
 % for ASM model
 model = 1;
-feat_names_new = [top_meds;{'baseline_sz_freq'};{'t_implant'}; {'ptID'}; {'seizure'}];
+feat_names_new = [top_meds; {'t_last_seizure'}; {'ptID'}; {'seizure'}]; 
 
 % save the coefficient estimates for each run
 [coeff_names,coeff_stats,all_coefs]  = model_bootstrap_stats(drug_table,feat_names_new,model,iter,ptIDs); % coef stats: mean, 95-CI
 
 disp('done with model coefficient estimates')
 %for null model
-null_names = [{'baseline_sz_freq'}; {'t_implant'}; {'ptID'}; {'seizure'}];
+null_names = [{'t_last_seizure'}; {'ptID'}; {'seizure'}];
 model=0;
 [coeff_names_null,coeff_stats_null,~] = model_bootstrap_stats(drug_table,null_names,model,iter,ptIDs);
 disp('done with null model coefficient estimates')
@@ -250,14 +285,14 @@ sig_coeffs = coeff_stats(:,end) <0.05;
 sig_coeffs_corrected = coeff_stats(:,end) <0.004;
 
 %
-plot(-1,inds(sig_coeffs),'*k');
+%plot(-1,inds(sig_coeffs),'*k');
 %plot(-0.5,inds(sig_coeffs_corrected),'*b'); % bonferroni corrected
 
 
 %% get AUC and operating point
 % estimates
 
-null_names = [{'baseline_sz_freq'};{'t_implant'}; {'ptID'}; {'seizure'}];
+null_names = [{'t_last_seizure'}; {'ptID'}; {'seizure'}];
 
 pts = 1:length(ptIDs);
 
@@ -268,15 +303,15 @@ for i = 1:iter
 end
 
 model = 1;
-[OPs,AUCs,Xroc,Yroc,conf_matrices]  = model_bootstrap_ROC(drug_table,feat_names,model,iter,pt_inds_master);
+[OPs,AUCs,Xroc,Yroc,conf_matrices]  = model_bootstrap_ROC(drug_table,feat_names_new,model,iter,pt_inds_master);
 disp('done with model AUCs')
 model =0;
 [OPs_null,AUCs_null,Xroc_null,Yroc_null,conf_matrices_null] =model_bootstrap_ROC(drug_table,null_names,model,iter,pt_inds_master);
 disp('done with null model AUCs')
 
 tail = 1; % one tailed p-value
-AUCout = bootstrap_ci_and_p(AUCs,AUCs_null);
-note = 'includes time since implant, and has all seizures (did not remove clustered seizures). null model has only baseline seizure frequency and time since implant';
+AUCout = bootstrap_ci_and_p(AUCs,AUCs_null)
+note = 'includes time since LAST SEIZURE, and has all seizures (did not remove lead seizures). null model has only time since LAST SEIZURE';
 % % save('all_coeffs_and_stats.mat');
 
 
